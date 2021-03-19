552637a17ee3b5f59a5fc9617f54078501dbf34e
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-03-18 22:06:31 -0600

    s9javascript

---
+++ s9javascript/js/main.js
@@ -0,0 +1,71 @@
+//alert("hola koders!!!")
+/*
+console.log("hola koders!!!")
+
+var variable1 = "Israel Salinas Martínez"
+
+console.log( typeof variable1 )
+
+var variable2 = 27
+
+console.log( typeof variable2 )
+
+var variable3 = { name:"israel Salinas martínez" }
+
+var variable4 = false
+
+var variable5;
+
+var variable6 = null;
+
+var variable7 = [
+    "a",
+    27,
+    {
+        name:"Israel"
+    }
+]
+
+console.log( typeof variable2 )
+
+console.log( typeof variable3 )
+
+console.log( typeof variable4 )
+
+console.log( typeof variable5 )
+console.log( variable5 )
+
+console.log( typeof variable6 )
+console.log( variable6 )
+
+console.log( typeof variable7 )
+
+*/
+
+/*
+    crear 2 variables
+    cada variable va a almacenar un número
+    imprimir en consola el resultado de cada una de las operaciones aritméticas entre esos números
+*/
+
+var numberOne = 8
+var numberTwo = 5
+
+var name =  "Israel"
+var lastName  = " Salinas Martinez"
+
+var additionResult = numberOne + numberTwo
+
+console.log( numberOne + numberTwo )
+
+document.write("<h1>" + ( numberOne + numberTwo ) + "</h1>")
+document.write("<h1>" + additionResult + "</h1>")
+
+document.write("<h2>" + name + lastName + "</h2>")
+
+document.writeln("<p>hola</p>");
+
+
+
+
+console.log(  )
