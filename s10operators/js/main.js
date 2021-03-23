d637ea5a89e151b2d7b5c2890f64debb23abe262
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-03-23 02:40:11 -0600

    s10operators

---
+++ s10operators/js/main.js
@@ -0,0 +1,40 @@
+var name = "Aria"
+
+var age = 25
+
+console.log( age )
+
+age += 3
+
+console.log( age )
+
+age += 12
+
+console.log( age )
+
+name += "Missett"
+
+console.log( name )
+
+name -= "Missett"
+
+console.log( name )
+
+var foo;
+
+console.log( !foo ) /*true*/
+
+foo = "foo"
+
+console.log( !foo ) /*false*/
+
+var obj = {
+    name:"Israel"
+}
+
+console.log( !obj.name )
+console.log( !obj.lastName )
+
+var myName = prompt("Ingresa tu nombre")
+var myAge = prompt("escribe tu edad")
+console.log( myName )
