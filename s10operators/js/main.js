8eef225b48b7e04532b0c7793215aaa849067ece
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-03-23 02:41:04 -0600

    s10operators

--- s10operators/js/main.js
+++ s10operators/js/main.js
@@ -38,3 +38,47 @@ console.log( !obj.lastName )
 var myName = prompt("Ingresa tu nombre")
 var myAge = prompt("escribe tu edad")
 console.log( myName )
+
+/*
+1 - Crear un script que reciba la dimensión de los 3 lados de un triángulo, y con base en la información recibida, determine si el triángulo en cuestión es equilátero, isóceles o escaleno. Informar el resultado mediante un alert.
+
+2 - Crear un script para recibir 2 de los lados de un cuadrilátero, y determinar si es un cuadrado o un rectángulo. Informar el resultado mediante un alert
+
+2.1 - Calcular el área del cuadrilátero en cuestión, e informarla a través de un alert
+
+3 - Crear un script que pida los datos necesarios para calcular el área de un triángulo, calcular el área y entregar el resultado mediante un alert.
+*/
+
+/*
+1. Crear un script para calcular el IMC (Indice de masa corporal) de cualquier persona, y determinar la composición corporal de dicha persona bajo los siguientes criterios:
+
+Peso inferior al normal	: IMC Menos de 18.5
+Normal	: IMC 18.5 – 24.9
+Peso superior al normal	: IMC 25.0 – 29.9
+Obesidad : IMC Más de 30.0
+
+2. Usando como referencia los siguientes datos de estaturas promedio:
+
+            General Varón   Mujer
+México	    169.9	170.0	160.8   cm's
+Australia	172.6	179.2	165.9   cm's
+Canadá	    171.0	178.1	163.9   cm's
+Brasil	    167.3	173.6	160.9   cm's
+Reino Unido	171.0	177.5	164.4   cm's
+
+crear un script que me permita conocer si mi estatura es superior, inferior o igual al promedio dependiendo de mi país y género. Si mi país no se encuentra en la lista, indicar que no se cuenta con el dato de estatura para ese país.
+
+3. Tomando como referencia los siguientes datos de densidades:
+
+Sustancia       Densidad (kg/m3)
+Acero           7850
+Cobre           8940
+Oro             19300
+Plata           10490
+Diamante        3515
+
+crear un script que me permita conocer el peso de un cubo de cualquier material (pedir al usuario la dimensión del lado del cubo y el material a consultar). Si el material no se encuentra en la lista, informar que no se cuentan con datos sobre la densidad de dicho material.
+
+*/
+
+
