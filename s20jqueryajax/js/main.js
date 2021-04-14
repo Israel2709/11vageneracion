01ae68e15a899f5af86f28409df2dd4bb9c4dda7
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-04-13 22:04:39 -0500

    jquery ajax

--- s20jqueryajax/js/main.js
+++ s20jqueryajax/js/main.js
@@ -49,10 +49,10 @@ const saveData = () => {
             band: "Theater of Tragedy",
             gender: "Gothic Metal"
         }),
-        success : response => {
+        success: response => {
             console.log(response)
         },
-        error : error => {
+        error: error => {
             console.log(error)
         }
     })
@@ -60,13 +60,13 @@ const saveData = () => {
 
 const deleteData = key => {
     $.ajax({
-        method:"DELETE",
-        url:`https://ajaxclass-1ca34.firebaseio.com/11g/israel/albums/${key}.json`,
+        method: "DELETE",
+        url: `https://ajaxclass-1ca34.firebaseio.com/11g/israel/albums/${key}.json`,
         success: response => {
-            console.log( response)
+            console.log(response)
         },
         error: error => {
-            console.log( error )
+            console.log(error)
         }
     })
 }
@@ -75,14 +75,14 @@ const deleteData = key => {
 
 const updateData = key => {
     $.ajax({
-        method:"PATCH",
-        url:`https://ajaxclass-1ca34.firebaseio.com/11g/israel/albums/${key}.json`,
-        data: JSON.stringify({ gender:"Symphonic Metal"}),
+        method: "PATCH",
+        url: `https://ajaxclass-1ca34.firebaseio.com/11g/israel/albums/${key}.json`,
+        data: JSON.stringify({ gender: "Symphonic Metal" }),
         success: response => {
-            console.log( response )
+            console.log(response)
         },
         error: error => {
-            console.log( error )
+            console.log(error)
         }
     })
 }
@@ -90,14 +90,14 @@ const updateData = key => {
 
 const putData = key => {
     $.ajax({
-        method:"PUT",
-        url:`https://ajaxclass-1ca34.firebaseio.com/11g/israel/albums/${key}.json`,
-        data: JSON.stringify({ gender:"Symphonic Metal"}),
+        method: "PUT",
+        url: `https://ajaxclass-1ca34.firebaseio.com/11g/israel/albums/${key}.json`,
+        data: JSON.stringify({ gender: "Symphonic Metal" }),
         success: response => {
-            console.log( response )
+            console.log(response)
         },
         error: error => {
-            console.log( error )
+            console.log(error)
         }
     })
 }
@@ -108,3 +108,72 @@ let cardHtml = `<div class="card">
     <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor est, maiores a illum quod laboriosam fugit ut esse aliquid quos?</div>
 </div>
 </div>`
+
+
+const getMentorData = () => {
+    let mentorObject = {}
+    $("form input").each(function (index) {
+        let property = this.name
+        let value = this.value
+
+        mentorObject[property] = value
+    });
+
+    saveMentor(mentorObject)
+}
+
+const saveMentor = mentorData => {
+    $.ajax({
+        method: "POST",
+        data: JSON.stringify(mentorData),
+        url: "https://ajaxclass-1ca34.firebaseio.com/11g/israel/mentors/.json",
+        success: response => {
+            console.log(response)
+            printMentors( getMentors() )
+        },
+        error: error => {
+            console.log(error)
+        }
+    })
+}
+
+$(".save-mentor").click(getMentorData)
+
+const getMentors = () => {
+    let mentorsCollection;
+    $.ajax({
+        method: "GET",
+        url: "https://ajaxclass-1ca34.firebaseio.com/11g/israel/mentors/.json",
+        success: response => {
+            mentorsCollection = response
+        },
+        async: false
+    })
+    return mentorsCollection
+}
+
+const printMentors = mentorsCollection => {
+    console.log(mentorsCollection)
+    $(".mentors-wrapper").empty()
+    for (mentor in mentorsCollection) {
+        let { name, email, phone } = mentorsCollection[mentor]
+        let mentorCard = `
+            <div class="col-6">
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="card-text">Nombre: ${name}</div>
+                        <div class="card-text">Email: ${email}</div>
+                        <div class="card-text">Phone: ${phone}</div>
+                        <div class="d-flex justify-content-between">
+                            <div class="btn btn-secondary">Eliminar</div>
+                            <div class="btn btn-primary">Editar</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        $(".mentors-wrapper").append(mentorCard)
+    }
+}
+
+printMentors(getMentors())
